e9b3ec22c62712e2ef2664f40ad6384669c366ac
Gareth Francis <gfrancis.dev@gmail.com>
2023-01-02 20:56:15 +0000

    build fixes

--- examples/features/composition_layer_quad/game.cpp
+++ examples/features/composition_layer_quad/game.cpp
@@ -11,6 +11,13 @@
 
 #include "interactions/interaction_teleport.h"
 
+std::vector<std::string> Game::requiredInstanceExtensions()
+{
+  return {
+    // vsgvr::KHRCompositionLayerEquirect2::instanceExtension(),
+  };
+}
+
 Game::Game(vsg::ref_ptr<vsgvr::Instance> xrInstance, vsg::ref_ptr<vsgvr::Viewer> vr, vsg::ref_ptr<vsg::Viewer> desktopViewer, bool displayDesktopWindow)
   : _xrInstance(xrInstance)
   , _vr(vr)
@@ -51,14 +58,43 @@ void Game::loadScene()
 
 void Game::initVR()
 {
+/* Skybox
+ * TODO: Only if extension is available from OpenXR runtime, with fallover to Equirect(1)
+ * TODO: Render from fixed image - Something pretty
+  auto placeholderImage = vsg::Image::create();
+  auto skyboxSpace = vsgvr::ReferenceSpace::create(_vr->getSession()->getSession(), XrReferenceSpaceType::XR_REFERENCE_SPACE_TYPE_STAGE);
+  _skyboxLayer = vsgvr::KHRCompositionLayerEquirect2::create(_vr->getInstance(), _vr->getSession(), _vr->getTraits(), skyboxSpace);
+  _skyboxLayer->widthPixels = 360;
+  _skyboxLayer->heightPixels = 180;
+
+  _skyboxLayer->radius = 100.0f;
+  // _skyboxLayer->upperVerticalAngle = 0.0;
+  _skyboxLayer->lowerVerticalAngle = - vsg::radians(10.0f); // The world has a ground plane, lower is pointless
+  // _skyboxLayer->centralHorizontalAngle = 0.0;
+
+  _skyboxLayer->clearColor = {1.0f, 0.0f, 1.0f, 1.0f};
+
+  // auto skyboxCommandGraph = buildSkyboxCommandGraph();
+  // skyboxLayer->assignRecordAndSubmitTask({skyboxCommandGraph});
+  auto skyboxCommandGraphs = _skyboxLayer->createCommandGraphsForImage(_vr->getSession(), placeholderImage);
+  _skyboxLayer->assignRecordAndSubmitTask(skyboxCommandGraphs);
+  _skyboxLayer->compile();
+  _vr->compositionLayers.push_back(_skyboxLayer);
+*/
+
   // Create CommandGraphs to render the scene to the HMD
   // TODO: This only really exists because vsg::createCommandGraphForView requires
   // a Window instance. Other than some possible improvements later, it could use the same code as vsg
   // OpenXR rendering may use one or more command graphs, as decided by the viewer
   // (TODO: At the moment only a single CommandGraph will be used, even if there's multiple XR views)
   // Note: assignHeadlight = false -> Scene lighting is required
-  auto headsetCompositionLayer = vsgvr::CompositionLayerProjection::create(_vr->getInstance(), _vr->getTraits(), _vr->getSession()->getSpace());
-  auto xrCommandGraphs = headsetCompositionLayer->createCommandGraphsForView(_vr->getSession(), _sceneRoot, _xrCameras, false);
+  auto headsetCompositionLayer = vsgvr::CompositionLayerProjection::create(_vr->referenceSpace);
+
+  // TODO: Only if skybox present, otherwise regular clear colour
+  headsetCompositionLayer->clearColor = {0.0f, 0.0f, 0.0f, 0.0f};
+  headsetCompositionLayer->flags = XR_COMPOSITION_LAYER_BLEND_TEXTURE_SOURCE_ALPHA_BIT | XR_COMPOSITION_LAYER_UNPREMULTIPLIED_ALPHA_BIT;
+
+  auto xrCommandGraphs = headsetCompositionLayer->createCommandGraphsForView(_vr->getInstance(), _vr->getSession(), _sceneRoot, _xrCameras, false);
   // TODO: This is almost identical to Viewer::assignRecordAndSubmitTaskAndPresentation - The only difference is
   // that OpenXRViewer doesn't have presentation - If presentation was abstracted we could avoid awkward duplication here
   headsetCompositionLayer->assignRecordAndSubmitTask(xrCommandGraphs);
@@ -66,46 +102,39 @@ void Game::initVR()
   _vr->compositionLayers.push_back(headsetCompositionLayer);
 
   // TODO: Quick hack to render <something> to a CompositionLayerQuad - This should do something better
-  {
-    auto lookAt = vsg::LookAt::create(vsg::dvec3(0.0, 0.0, 10.0), vsg::dvec3(0.0, 0.0, 0.0), vsg::dvec3(0.0, -1.0, 0.0));
-    // Camera parameters as if it's rendering to a desktop display, appropriate size for the displayed quad
-    auto perspective = vsg::Perspective::create(30.0, 1920.0 / 1080.0, 0.1, 100.0);
 
-    // Configure rendering from an overhead camera, displaying the scene
-    // - A camera is not require here, but is required if the application later wants a reference to it
-    // - The composition layer's basic parameters (pose, scale) may be modified later at any time
-    // - There is a runtime-specific limit to the number of composition layers. Only a few should be used, if more than one.
-    auto overheadCamera = vsg::Camera::create(perspective, lookAt, vsg::ViewportState::create(0, 0, 1920, 1080));
+  auto lookAt = vsg::LookAt::create(vsg::dvec3(0.0, 0.0, 10.0), vsg::dvec3(0.0, 0.0, 0.0), vsg::dvec3(0.0, -1.0, 0.0));
+  // Camera parameters as if it's rendering to a desktop display, appropriate size for the displayed quad
+  auto perspective = vsg::Perspective::create(30.0, 1920.0 / 1080.0, 0.1, 100.0);
 
-    // A quad positioned in the world (scene reference space)
-    // auto quadLayer = vsgvr::CompositionLayerQuad::create(_vr->getInstance(), _vr->getTraits(), _vr->getSession()->getSpace(), 1920, 1080);
-    // quadLayer->setPose(
-      // {0.0, 4.0, 1.0},
-      // vsg::dquat(vsg::radians(25.0), {1.0, 0.0, 0.0})
-    // );
+  // Configure rendering from an overhead camera, displaying the scene
+  // - A camera is not require here, but is required if the application later wants a reference to it
+  // - The composition layer's basic parameters (pose, scale) may be modified later at any time
+  // - There is a runtime-specific limit to the number of composition layers. Only a few should be used, if more than one.
+  auto overheadCamera = vsg::Camera::create(perspective, lookAt, vsg::ViewportState::create(0, 0, 1920, 1080));
 
-    // A quad positioned in front of the user's face
-    auto faceLockedSpace = vsgvr::ReferenceSpace::create(_vr->getSession()->getSession(), XrReferenceSpaceType::XR_REFERENCE_SPACE_TYPE_VIEW);
+  /*
+  // A quad positioned in the world (scene reference space)
+  auto quadLayer = vsgvr::CompositionLayerQuad::create(_vr->getInstance(), _vr->getSession(), _vr->getTraits(), _vr->getSession()->getSpace(), 1920, 1080);
+  quadLayer->setPose(
+    {0.0, 4.0, 1.0},
+    vsg::dquat(vsg::radians(25.0), {1.0, 0.0, 0.0})
+  );
+  quadLayer->sizeMeters.width = 1.920;
+  quadLayer->sizeMeters.height = 1.080;
 
-    auto quadLayer = vsgvr::CompositionLayerQuad::create(_vr->getInstance(), _vr->getTraits(), faceLockedSpace, 1920, 1080);
-    quadLayer->setPose(
-      {0.0, 4.0, 0.0},
-      {0.0, 0.0, 0.0, 1.0}
-    );
-
-    /*auto rot = vsg::quat({0.0f, 5.0f, 2.5f}, {0.0f, 0.0f, 2.5f});
-    quadLayer->pose.orientation = {
-      rot.x, rot.y, rot.z, rot.w
-    };*/
-    // Quad size taking in to account aspect ratio
-    quadLayer->sizeMeters = { 1.920f, 1.080f };
-
-    std::vector<vsg::ref_ptr<vsg::Camera>> cameras = { overheadCamera };
-    auto overheadCommandGraphs = quadLayer->createCommandGraphsForView(_vr->getSession(), _sceneRoot, cameras, false);
-    quadLayer->assignRecordAndSubmitTask(overheadCommandGraphs);
-    quadLayer->compile();
-    _vr->compositionLayers.push_back(quadLayer);
-  }
+  // Pass a RenderGraph to the composition layer
+  // This must _not_ have a window or view assigned - Windows are not relevant for OpenXR
+  // rendering, rather OpenXR swapchains are acquired/released for each frame
+  auto quadRenderGraph = vsg::RenderGraph::create();
+  auto renderImgui = vsgvr::RenderImGui::create(_vr->getGraphicsBinding(), VkExtent2D{1920, 1080}, true);
+  renderImgui->add(_ui);
+  quadRenderGraph->addChild(renderImgui);
+  auto quadCommandGraphs = quadLayer->createCommandGraphsForRenderGraph(_vr->getSession(), quadRenderGraph);
+  quadLayer->assignRecordAndSubmitTask({quadCommandGraphs});
+  quadLayer->compile();
+  _vr->compositionLayers.emplace_back(quadLayer);
+  */
 
   if(_desktopWindowEnabled)
   {
@@ -127,7 +156,7 @@ void Game::initActions()
 {
   // Tracking the location of the user's headset is achieved by tracking the VIEW reference space
   // This may be done by the application, but vsgvr::Viewer can also track a set of SpaceBindings per-frame (within the session's reference space)
-  _headPose = vsgvr::SpaceBinding::create(vsgvr::ReferenceSpace::create(_vr->getSession()->getSession(), XrReferenceSpaceType::XR_REFERENCE_SPACE_TYPE_VIEW));
+  _headPose = vsgvr::SpaceBinding::create(vsgvr::ReferenceSpace::create(_vr->getSession(), XrReferenceSpaceType::XR_REFERENCE_SPACE_TYPE_VIEW));
   _vr->spaceBindings.push_back(_headPose);
 
   // Input devices are tracked via ActionPoseBindings - Tracking elements from the OpenXR device tree in the session space,
@@ -172,7 +201,7 @@ void Game::initActions()
   {
     if (! vsgvr::ActionSet::suggestInteractionBindings(_xrInstance, p.first, p.second))
     {
-      throw vsg::Exception({ "Failed to configure interaction bindings for controllers" });
+      throw std::runtime_error({ "Failed to configure interaction bindings for controllers" });
     }
   }
 
