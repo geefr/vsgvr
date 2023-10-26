8c44e4244b937f8c58fc6a561bc600eba6877bb3
Ralf Habacker <ralf.habacker@freenet.de>
2023-10-26 23:49:42 +0200

    Reorder vsg include header alphabetical

--- vsgvr/src/vsgvr/app/CompositionLayer.cpp
+++ vsgvr/src/vsgvr/app/CompositionLayer.cpp
@@ -31,9 +31,9 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <vsg/app/View.h>
 #include <vsg/app/RenderGraph.h>
+#include <vsg/commands/PipelineBarrier.h>
 #include <vsg/ui/FrameStamp.h>
 #include <vsg/vk/SubmitCommands.h>
-#include <vsg/commands/PipelineBarrier.h>
 
 #include "../xr/Macros.cpp"
 
